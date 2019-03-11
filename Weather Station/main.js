14472a3dec911426761ce60eb17015e017ea4361
Patrick Audriaz <patrick.audriaz@edu.hefr.ch>
2019-03-11 15:53:15 +0100

    api get

--- Weather Station/main.js
+++ Weather Station/main.js
@@ -1,60 +1,86 @@
-var sta1 = false;
-var sta2 = false;
-var sta3 = false;
+var stations = ["Posieux", "MolésonVillage", "Zermatt"];
+var stationNbr = stations.length;
+var stationsState = Array(stationNbr).fill(false);
+var timers = Array(stationNbr).fill(null);
+
+var allURL = "http://appint01.tic.heia-fr.ch/";
+
+var delay = 2000;
 
 $(document).ready(function() {
-  function getWeatherStationData() {}
+  console.log("Document is ready !");
+
+  // call to start loop
+  timer = setInterval(function() {
+    console.log("tick");
+  }, delay);
+
+  // call to top loop
+  clearInterval();
+  timer = null;
 });
 
-function updateDisplay(name) {
-  if (name === "sta1") {
-    if (sta1 == false) {
-      sta1 = true;
-      document.getElementById("sta1").classList.remove("carousel-item-name");
-      document
-        .getElementById("sta1")
-        .classList.add("carousel-item-name-selected");
-    } else {
-      sta1 = false;
-      document
-        .getElementById("sta1")
-        .classList.remove("carousel-item-name-selected");
-      document.getElementById("sta1").classList.add("carousel-item-name");
-    }
-    console.log("sta1 : ", sta1);
-  }
-  if (name === "sta2") {
-    if (sta2 == false) {
-      sta2 = true;
-      document.getElementById("sta2").classList.remove("carousel-item-name");
-      document
-        .getElementById("sta2")
-        .classList.add("carousel-item-name-selected");
-    } else {
-      sta2 = false;
-      document
-        .getElementById("sta2")
-        .classList.remove("carousel-item-name-selected");
-      document.getElementById("sta2").classList.add("carousel-item-name");
-    }
-    console.log("sta2 : ", sta2);
-  }
+getWeatherStationData();
 
-  if (name === "sta3") {
-    if (sta3 == false) {
-      sta3 = true;
-      document.getElementById("sta3").classList.remove("carousel-item-name");
-      document
-        .getElementById("sta3")
-        .classList.add("carousel-item-name-selected");
-    } else {
-      sta3 = false;
-      document
-        .getElementById("sta3")
-        .classList.remove("carousel-item-name-selected");
-      document.getElementById("sta3").classList.add("carousel-item-name");
+function getWeatherStationData() {
+  var requestURL = allURL;
+  var stationsGET = [];
+
+  $.ajax({
+    method: "GET",
+    url: requestURL,
+    headers: {
+      Accept: "application/json"
+      //"Content-type" : "application/json"
+    },
+
+    success: function(data) {
+      //console.log(JSON.stringify(data.stations));
+      stationsGET = data;
+      $.each(data.stationsGET, function(index, value) {
+        console.log(
+          "Datas from stations located at " +
+            JSON.stringify(value.location) +
+            " are ready !"
+        );
+        console.log(data.stationsGET[index]);
+        console.log(data.stationsGET.length);
+        updateDisplay(value);
+      });
+    },
+
+    error: function(xhr, status, error) {
+      alert("error");
+      console.log("xhr: " + xhr);
+      console.log("status: " + status);
+      console.log("error: " + error);
     }
-    console.log("sta3 : ", sta3);
+  });
+}
+
+function updateDisplay(name) {
+  console.log(name);
+  for (var i = 0; i < stationNbr; i++) {
+    if (name === stations[i]) {
+      if (stationsState[i] == false) {
+        stationsState[i] = true;
+        document
+          .getElementById(stations[i])
+          .classList.remove("carousel-item-name");
+        document
+          .getElementById("sta1")
+          .classList.add("carousel-item-name-selected");
+      } else {
+        stations[i] = false;
+        document
+          .getElementById(stations[i])
+          .classList.remove("carousel-item-name-selected");
+        document
+          .getElementById(stations[i])
+          .classList.add("carousel-item-name");
+      }
+      console.log(stations[i] + stationsState[i]);
+    }
   }
 }
 
@@ -63,7 +89,7 @@ $(function() {
 });
 
 window.onload = function() {
-  var ctx = document.getElementById("line_chart_temp_sta1");
+  var ctx = document.getElementById("line_chart_temp");
   var line_chart_temp = new Chart(ctx, {
     type: "line",
     data: {
@@ -129,7 +155,7 @@ window.onload = function() {
     }
   });
 
-  var ctx2 = document.getElementById("line_chart_hum_sta1");
+  var ctx2 = document.getElementById("line_chart_hum");
   var line_chart_hum = new Chart(ctx2, {
     type: "line",
     data: {
@@ -195,7 +221,7 @@ window.onload = function() {
     }
   });
 
-  var ctx3 = document.getElementById("line_chart_press_sta1");
+  var ctx3 = document.getElementById("line_chart_press");
   var line_chart_press = new Chart(ctx3, {
     type: "line",
     data: {
@@ -224,215 +250,6 @@ window.onload = function() {
             1013.3,
             1018,
             1015.4,
-            1013.3
-          ],
-          borderColor: "#ecb984",
-          fontColor: "#DEDEDE",
-          fill: false
-        }
-      ]
-    },
-
-    options: {
-      title: {
-        display: false
-      },
-
-      legend: {
-        labels: {
-          fontColor: "#DEDEDE"
-        }
-      },
-
-      scales: {
-        scaleLabel: {
-          fontColor: "#DEDEDE"
-        },
-
-        xAxes: [
-          {
-            gridLines: {
-              display: false,
-              color: "#DEDEDE",
-              lineWidth: 2
-            }
-          }
-        ],
-
-        yAxes: [
-          {
-            gridLines: {
-              display: false,
-              color: "#DEDEDE",
-              lineWidth: 2
-            }
-          }
-        ]
-      }
-    }
-  });
-
-  var ctx = document.getElementById("line_chart_temp_sta2");
-  var line_chart_temp = new Chart(ctx, {
-    type: "line",
-    data: {
-      labels: [
-        22.02,
-        23.02,
-        24.02,
-        25.02,
-        26.02,
-        27.02,
-        28.02,
-        1.03,
-        2.03,
-        3.03
-      ],
-      datasets: [
-        {
-          label: "Temperature [˚C]",
-          data: [-10, -8, -3, 0, 0, 5, 7, 3, 10, 15],
-          borderColor: "#8BC6EC",
-          fontColor: "#DEDEDE",
-          fill: false
-        }
-      ]
-    },
-
-    options: {
-      title: {
-        display: false
-      },
-
-      legend: {
-        labels: {
-          fontColor: "#DEDEDE"
-        }
-      },
-
-      scales: {
-        scaleLabel: {
-          fontColor: "#DEDEDE"
-        },
-
-        xAxes: [
-          {
-            gridLines: {
-              display: false,
-              color: "#DEDEDE",
-              lineWidth: 2
-            }
-          }
-        ],
-
-        yAxes: [
-          {
-            gridLines: {
-              display: false,
-              color: "#DEDEDE",
-              lineWidth: 2
-            }
-          }
-        ]
-      }
-    }
-  });
-
-  var ctx2 = document.getElementById("line_chart_hum_sta2");
-  var line_chart_hum = new Chart(ctx2, {
-    type: "line",
-    data: {
-      labels: [
-        22.02,
-        23.02,
-        24.02,
-        25.02,
-        26.02,
-        27.02,
-        28.02,
-        1.03,
-        2.03,
-        3.03
-      ],
-      datasets: [
-        {
-          label: "Humidité [%]",
-          data: [37, 40, 44, 48, 39, 45, 43, 40, 36, 40],
-          borderColor: "#b68aec",
-          fontColor: "#DEDEDE",
-          fill: false
-        }
-      ]
-    },
-
-    options: {
-      title: {
-        display: false
-      },
-
-      legend: {
-        labels: {
-          fontColor: "#DEDEDE"
-        }
-      },
-
-      scales: {
-        scaleLabel: {
-          fontColor: "#DEDEDE"
-        },
-
-        xAxes: [
-          {
-            gridLines: {
-              display: false,
-              color: "#DEDEDE",
-              lineWidth: 2
-            }
-          }
-        ],
-
-        yAxes: [
-          {
-            gridLines: {
-              display: false,
-              color: "#DEDEDE",
-              lineWidth: 2
-            }
-          }
-        ]
-      }
-    }
-  });
-
-  var ctx3 = document.getElementById("line_chart_press_sta2");
-  var line_chart_press = new Chart(ctx3, {
-    type: "line",
-    data: {
-      labels: [
-        22.02,
-        23.02,
-        24.02,
-        25.02,
-        26.02,
-        27.02,
-        28.02,
-        1.03,
-        2.03,
-        3.03
-      ],
-      datasets: [
-        {
-          label: "Pression [hPa]",
-          data: [
-            1014,
-            1012.8,
-            1016,
-            1018,
-            1012,
-            1013.4,
-            1013.3,
-            1016.2,
-            1011.4,
             1013.3
           ],
           borderColor: "#ecb984",
